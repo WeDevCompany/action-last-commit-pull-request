e053b80ff0d6b9e69ec3f1aec998ad9ede72a63f
Sikay <pedrohdezmora@gmail.com>
2020-07-08 21:08:31 +0200

    WIP: prueba de valores

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo ${INPUT_TOKEN}
-echo ${INPUT_PULLREQUEST}
-echo "curl -i -H Authorization: token ${INPUT_TOKEN} ${INPUT_PULLREQUEST}/commits?page=last | jq '[.[] ] | last'"
+LAST_PR=$(curl -H "Authorization: token ${INPUT_TOKEN}" ${INPUT_PULLREQUEST}/commits?page=last | jq '[.[] ] | last')
 
-LAST_PR=$(curl -i -H "Authorization: token ${INPUT_TOKEN}" ${INPUT_PULLREQUEST}/commits?page=last | jq '[.[] ] | last')
-
-COMMIT_MESSAGE=$LAST_PR | jq '.commit["message"]'
+COMMIT_MESSAGE=$(echo $LAST_PR | jq '.commit["message"]')
 
 
 echo $COMMIT_MESSAGE
